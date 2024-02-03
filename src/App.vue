6de5e329768c8e8f668215ebd6a023e5d6babbcc
Md Anikul Islam Binju <anikul.islam.binju@gmail.com>
2024-02-03 23:17:43 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <!-- Navbar -->
+    <NavBar v-if="loggedInStatus"></NavBar>
+    <!-- Main Sidebar -->
+    <SideBar v-if="loggedInStatus"></SideBar>
+
+    <!-- Content page -->
+    <router-view></router-view>
+
+
+
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideBar,
+    NavBar
+  },
+  computed:{
+    loggedInStatus(){
+     return this.$store.getters.GET_AUTH_STATUS;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
